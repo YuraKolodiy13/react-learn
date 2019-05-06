3e7b4f175681291109424c999ea07cefe4613786
YuraKolodiy13 <Timberwolves13>
2019-05-06 16:45:56 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>some</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus, aperiam, at aut dolorem dolores enim est eum explicabo fugit harum hic magni nihil odio optio quam repudiandae sequi velit!</p>
     </div>
   );
 }
