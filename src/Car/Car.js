c19d0c97c95c2d9b62954d5fe464b61d58f75d5b
YuraKolodiy13 <Timberwolves13>
2019-05-07 17:50:27 +0300

    add something new

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -4,7 +4,7 @@ export default props => (
     <div style={{border: '1px solid red'}}>
       <h3>Car name: {props.name}</h3>
       <p>Year: <strong>{props.year}</strong></p>
-      {/*<input type="text" onChange={props.onChangeTitle}/>*/}
-      {/*<button onClick={props.onChangeTitle}>click</button>*/}
+      <input type="text" onChange={props.onChangeTitle}/>
+      <button onClick={props.onChangeTitle}>click</button>
     </div>
 )
