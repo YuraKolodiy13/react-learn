6a8b0c008464b0f4c094865b7a30342251da7457
YuraKolodiy13 <Timberwolves13>
2019-05-07 17:14:47 +0300

    work more

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import './App.css';
+import Car from './Car/Car';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>some</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus, aperiam, at aut dolorem dolores enim est eum explicabo fugit harum hic magni nihil odio optio quam repudiandae sequi velit!</p>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    cars: [
+      {name: 'mazda', year: 2018},
+      {name: 'doodgee', year: 2118},
+      {name: 'rover', year: 2008}
+    ],
+    pageTitle: "let's go man",
+    showCar: false
+  };
+
+  toggleHandler = () => {
+    this.setState({
+      showCar: !this.state.showCar
+    })
+  };
+  changeTitleHandler = (e) => {
+    this.setState({
+      pageTitle: e.target.value
+    })
+  };
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <p>{this.state.pageTitle}</p>
+            <button onClick={this.toggleHandler}>toggle cars</button>
+
+            {this.state.showCar ?
+                this.state.cars.map((item, i) => {
+                  return (
+                      <Car key={i} name={item.name} year={item.year} onChangeTitle={this.changeTitleHandler}/>
+                  )
+                }) :
+                null}
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Car/Car.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <div style={{border: '1px solid red'}}>
+      <h3>Car name: {props.name}</h3>
+      <p>Year: <strong>{props.year}</strong></p>
+      {/*<input type="text" onChange={props.onChangeTitle}/>*/}
+      {/*<button onClick={props.onChangeTitle}>click</button>*/}
+    </div>
+)
